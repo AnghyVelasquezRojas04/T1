6f8df28006aa04838c9d0808931d927be71f31cc
AnghyVelasquezRojas04 <142699682+AnghyVelasquezRojas04@users.noreply.github.com>
2023-09-09 13:32:16 -0500

    Add files via upload

---
+++ src/t1/T1.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package t1;
+
+/**
+ *
+ * @author anghy
+ */
+public class T1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Lista screen = new Lista();
+        screen.setLocationRelativeTo(null);
+        screen.setVisible(true);
+    }
+    
+}
